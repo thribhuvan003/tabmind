fc62e80e72e3667600262df7f7ac9081c947b32e
thribhuvan003 <nothing007544@gmail.com>
2026-05-06 17:51:33 +0530

    background worker with alarm-based snapshots

---
+++ extension/entrypoints/background/index.ts
@@ -0,0 +1,25 @@
+﻿import { runSessionSnapshot } from "../../lib/session-engine";
+import { storageSet } from "../../lib/storage";
+
+export default defineBackground(() => {
+  chrome.runtime.onInstalled.addListener(async () => {
+    await storageSet("tabmind:session:startedAt", Date.now());
+    chrome.alarms.create("tabmind:snapshot", { periodInMinutes: 1.5 });
+  });
+
+  chrome.alarms.onAlarm.addListener(async (alarm) => {
+    if (alarm.name !== "tabmind:snapshot") return;
+    try {
+      await runSessionSnapshot();
+    } catch (err) {
+      console.error("[TabMind] snapshot error:", err);
+    }
+  });
+
+  chrome.runtime.onMessage.addListener((msg, _sender, sendResponse) => {
+    if (msg.type === "TABMIND_SNAPSHOT_NOW") {
+      runSessionSnapshot().then(sendResponse).catch(() => sendResponse(null));
+      return true;
+    }
+  });
+});
