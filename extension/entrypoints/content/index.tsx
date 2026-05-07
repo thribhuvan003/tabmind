40effc6056871854e3c566df9ab252e664117e85
thribhuvan003 <nothing007544@gmail.com>
2026-05-07 16:55:02 +0530

    content script: inject widget on every page

---
+++ extension/entrypoints/content/index.tsx
@@ -0,0 +1,24 @@
+﻿import ReactDOM from "react-dom/client";
+import { Widget } from "../../components/widget/Widget";
+
+export default defineContentScript({
+  matches: ["<all_urls>"],
+  cssInjectionMode: "ui",
+  async main(ctx) {
+    const ui = await createShadowRootUi(ctx, {
+      name: "tabmind-widget",
+      position: "overlay",
+      anchor: "body",
+      append: "last",
+      onMount(container) {
+        const root = ReactDOM.createRoot(container);
+        root.render(<Widget />);
+        return root;
+      },
+      onRemove(root) {
+        root?.unmount();
+      },
+    });
+    ui.mount();
+  },
+});
